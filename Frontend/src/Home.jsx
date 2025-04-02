4b1659ce5d12f97b6bc8e532c86eb8c6e720ed0e
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-04-02 14:29:54 +0530

    test

---
+++ Frontend/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Rohitha</h1>
+        <h2>gannamaneedi.rohitha.s89@kalvium.community</h2>
+    </div>
+  )
+}
+
+export default Home
